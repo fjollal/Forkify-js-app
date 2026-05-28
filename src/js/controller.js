4a92419ce9a79c9d99c728258a6656a1634287ac
fjollal <fl57305@ubt-uni.net>
2026-05-28 15:44:36 +0200

    html and css

---
+++ src/js/controller.js
@@ -0,0 +1,9 @@
+const receipeContainer=document.querySelector('.recipe');
+
+const timeout=function(s){
+    return new Promise(function(_,reject){
+        setTimeout(function(){
+            reject(new Error(`Request took too long! Timeout after ${s} seconds`));
+        }, s * 1000)
+    });
+};
